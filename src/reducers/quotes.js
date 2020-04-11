bbebaccc7d6e109ab3fce111f139ae0636eb304e
scrabill <shannon.crabill@gmail.com>
2020-04-11 19:33:59 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,9 +1,15 @@
 export default (state = [], action) => {
+  // let index
+  // let quote
+
   switch (action.type) {
     case 'ADD_QUOTE':
       return state.concat(action.quote)
     case 'REMOVE_QUOTE':
       return state.filter(quote => quote.id !== action.quoteId)
+    case 'UPDATE_VOTE':
+      let quote = state.filter(quote => quote.id === action.quoiteId)
+      return quote.votes += 1
     default:
       state
   }
